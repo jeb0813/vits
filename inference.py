2ef114db8d0eac4ba15cb5c7ab5066e62410200c
jeb0813 <57085763+jeb0813@users.noreply.github.com>
2023-10-17 13:37:40 +0800

    basic infer func

---
+++ inference.py
@@ -0,0 +1,45 @@
+import os
+import json
+import math
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+
+import commons
+import utils
+from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+
+from scipy.io.wavfile import write
+
+# 文本预处理
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm)
+    return text_norm
+
+# 配置加载和生成器初始化
+hps = utils.get_hparams_from_file("./configs/ljs_base.json")
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    **hps.model).cuda()
+_ = net_g.eval()
+# config path here
+_ = utils.load_checkpoint("./logs/ljs_base/G_320000.pth", net_g, None)
+
+
+stn_tst = get_text("VITS is Awesome!", hps)
+with torch.no_grad():
+    x_tst = stn_tst.cuda().unsqueeze(0)
+    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    output_file = "./output.wav"
+    write(output_file, 22050, audio)
+# ipd.display(ipd.Audio(audio, rate=hps.data.sampling_rate, normalize=False))
